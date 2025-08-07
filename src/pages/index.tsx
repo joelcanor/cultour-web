5ae1719d57dd6cb60e2b0548c64508f6a44de151
canitobaby1234 <161373665+canitobaby1234@users.noreply.github.com>
2025-08-07 00:30:01 -0600

    prueba imagenes 1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -126,7 +126,7 @@ const InfiniteCarousel = ({ places, favoritos, toggleFavorito }: CarouselProps) 
             }}
           >
             <Image
-              src={place.url_imagen.replace('yiaplotfopkyasrkhjhf', 'yiaplofsopkyasrkhjhf').replace('//', '/')}
+            src={place.url_imagen}
               alt={place.nombre}
               width={280}
               height={200}
@@ -1304,7 +1304,7 @@ export default function Home() {
                         </div>
 
                         <Image
-                          src={place.url_imagen.replace('yiaplotfopkyasrkhjhf', 'yiaplofsopkyasrkhjhf').replace('//', '/')}
+                          src={place.url_imagen}
                           alt={place.nombre}
                           width={400}
                           height={220}
