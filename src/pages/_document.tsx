0e99dca0de189fe12ee4cf16db8544977afc3bd2
canitobaby1234 <161373665+canitobaby1234@users.noreply.github.com>
2025-08-14 15:36:39 -0600

    Implementación PWA

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,17 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Manifest PWA */}
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* Color del navegador */}
+        <meta name="theme-color" content="#317EFB" />
+
+        {/* Íconos principales */}
+        <link rel="icon" href="/icons/icon-192x192.png" />
+        <link rel="apple-touch-icon" href="/icons/icon-512x512.png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
